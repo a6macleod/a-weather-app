06deec73ca20157312677a0056b7e55234f21673
Andrew MacLeod <A6macleod@gmail.com>
2020-03-11 20:28:18 -0400

    style and layout CSS

--- src/script.js
+++ src/script.js
@@ -4,10 +4,13 @@ const searchCityForm = document.querySelector('.search_city_form');
 const searchCityField = document.querySelector('#search_city_field');
 
 const displayCity = document.querySelector('.city');
-const displaySky = document.querySelector('.sky');
+const displayWeatherDescription = document.querySelector('.sky');
 const displayCurrentTemp = document.querySelector('.current_temp');
 const displayTempMax = document.querySelector('.temp_max');
 const displayTempMin = document.querySelector('.temp_min');
+const displayIcon = document.querySelector('.weather_icon_display'); // weather icon
+
+const changeUnitsButton = document.querySelector('.change_units');
 
 let cityName = 'CHICAGO';
 const fahrenheit = 'units=imperial';
@@ -17,13 +20,106 @@ let cityData;
 
 const symbols = '!@#$%^&*(){}/?,<>`~|[]';
 
-function validationError(errorType) {
-  console.log(`error: ${errorType}`);
-  // code for code for adding an error for submitting a request with a symbol
+function clearDisplay() {
+  displayCity.innerHTML = '';
+  displayWeatherDescription.innerHTML = '';
+  displayCurrentTemp.innerHTML = '';
+  displayTempMax.innerHTML = '';
+  displayTempMin.innerHTML = '';
+  while (displayIcon.firstChild) {
+    displayIcon.removeChild(displayIcon.firstChild);
+  } // Weather Icon - check if working
 }
 
 function removeError() {
-  // code to remove the error
+  const errorMessage = document.querySelector('.error_message');
+  // check if errormessage is there and if so remove it
+  if (errorMessage) {
+    errorMessage.remove(errorMessage);
+  }
+}
+
+function validationError(errorType) {
+  cityData = {};
+  let errorSource;
+  clearDisplay();
+
+  if (errorType === 'blank') {
+    errorSource = 'a blank submission';
+  } else if (errorType === 'symbol') {
+    errorSource = 'a strange symbol in your city name';
+  } else {
+    errorSource = 'an error getting the info';
+  }
+
+  const mainDiv = document.querySelector('.display');
+
+  // check if there is an error message already and remove it
+  const errorMessage = document.querySelector('.error_message');
+  if (errorMessage) {
+    removeError();
+  }
+
+  const header = document.createElement('h1');
+  header.classList.add('error_message');
+  header.innerHTML = `Whoops something went wrong because of ${errorSource}. Try entering another city`;
+  mainDiv.prepend(header);
+}
+
+function loadIcon() {
+  const image = document.createElement('img');
+  image.classList.add('icon');
+  image.setAttribute('src', `http://openweathermap.org/img/wn/${cityData.weatherIcon}@2x.png`);
+  displayIcon.appendChild(image);
+}
+
+function displayData() {
+  let displayUnits;
+  if (units === fahrenheit) {
+    displayUnits = '℉';
+  } else {
+    displayUnits = '℃';
+  }
+
+  displayCity.innerHTML = cityData.city;
+  displayWeatherDescription.innerHTML = cityData.weatherDescription;
+  displayCurrentTemp.innerHTML = `${cityData.currentTemp} ${displayUnits}`;
+  displayTempMax.innerHTML = `${cityData.maxTemp}`;
+  displayTempMin.innerHTML = `${cityData.minTemp} ${displayUnits}`;
+  loadIcon();
+}
+
+function loadCityData(data) {
+  clearDisplay();
+  searchCityField.value = '';
+  console.log(data);
+  cityData = {
+    city: data.name,
+    currentTemp: data.main.temp,
+    maxTemp: data.main.temp_max,
+    minTemp: data.main.temp_min,
+    weatherMain: data.weather[0].main,
+    weatherDescription: data.weather[0].description,
+    weatherIcon: data.weather[0].icon,
+  };
+  displayData();
+}
+
+async function getWeatherData() {
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&${units}&appid=1b1a6dceff48a9792acfdc16ccc5da0e`,
+      { mode: 'cors' });
+    const weatherData = await response.json();
+    loadCityData(weatherData);
+  } catch(error) {
+    validationError(error);
+    console.log(error);
+  }
+}
+
+function findCity(validatedCity) {
+  cityName = validatedCity;
+  getWeatherData();
 }
 
 function validateSymbols(cityEntered) {
@@ -34,53 +130,6 @@ function validateSymbols(cityEntered) {
     }
   }
   return status;
-}
-
-function loadCityData(data) {
-  cityData = {
-    city: data.name,
-    currentTemp: data.main.temp,
-    maxTemp: data.main.temp_max,
-    minTemp: data.main.temp_min,
-    sky: data.weather[0].main,
-  };
-  display();
-  console.log(cityData);
-}
-
-async function getWeatherData() {
-  try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&${units}&appid=1b1a6dceff48a9792acfdc16ccc5da0e`,
-      { mode: 'cors' });
-    const weatherData = await response.json();
-    loadCityData(weatherData);
-  }
-  catch(error) {
-    validationError('server');
-    console.log(error);
-    // catch error
-  }
-}
-
-function display() {
-  let displayUnits;
-
-  if (units === fahrenheit) {
-    displayUnits = '℉';
-  } else {
-    displayUnits = '℃';
-  }
-
-  displayCity.innerHTML = cityData.city;
-  displaySky.innerHTML = cityData.sky;
-  displayCurrentTemp.innerHTML = `${cityData.currentTemp} ${displayUnits}`;
-  displayTempMax.innerHTML = `${cityData.maxTemp} ${displayUnits}`;
-  displayTempMin.innerHTML = `${cityData.minTemp} ${displayUnits}`;
-}
-
-function findCity(validatedCity) {
-  cityName = validatedCity;
-  getWeatherData();
 }
 
 function validateFormData(e) {
@@ -96,6 +145,16 @@ function validateFormData(e) {
   }
 }
 
-getWeatherData();
+function changeUnits() {
+  if (units === fahrenheit) {
+    units = celcius;
+  } else {
+    units = fahrenheit;
+  }
+  getWeatherData();
+}
 
 searchCityForm.addEventListener('submit', validateFormData);
+changeUnitsButton.addEventListener('click', changeUnits);
+
+getWeatherData();
